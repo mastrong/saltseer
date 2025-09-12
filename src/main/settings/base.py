ff2d298f9bd0c5751eed740877f1c62629e3b8ec
Melissa Strong <melissastrong89@gmail.com>
2025-09-12 17:31:19 -0400

    change to pgsql; add AbstractUser

--- src/main/settings/base.py
+++ src/main/settings/base.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,17 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+qdj_24qsr^+jd(t$zp+t9blr_yni+(t&^@uh1d(8mpcb)!-04'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+    'main',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('POSTGRES_DB'),  # Replace with your PostgreSQL database name
+        'USER': os.getenv('POSTGRES_USER'),  # Replace with your PostgreSQL username
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),  # Replace with your PostgreSQL password
+        'HOST': os.getenv('DB_HOST'),  # Or the hostname/IP of your PostgreSQL server
+        'PORT': os.getenv('DB_PORT'),  # Default PostgreSQL port, change if different
     }
 }
 
@@ -104,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = os.getenv('TIME_ZONE')
 
 USE_I18N = True
 
@@ -120,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.SaltUser'
